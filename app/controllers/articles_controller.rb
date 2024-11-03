c5c1d38b2c371abe620afc4c68efbc1f6dc45514
FameNu <110712106+FameNu@users.noreply.github.com>
2024-11-03 11:31:49 +0700

    Add new action to ArticlesController for creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # GET /articles/new -> open view form
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles -> create new article
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
